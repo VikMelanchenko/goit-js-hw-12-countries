8e752cb20f01397365db82ea0d83c4237dc215f0
VikMelanchenko <63492650+VikMelanchenko@users.noreply.github.com>
2020-11-13 02:16:11 +0200

    add js fetch

--- js/fetchCountries.js
+++ js/fetchCountries.js
@@ -1,7 +1,7 @@
 const BASE_URL = 'https://restcountries.eu/rest/v2/name';
     
-function fetchCountry(countryName) {
-    return fetch(`${BASE_URL}/${countryName}`).then(response => response.json());
+function fetchCountry(searchQuery) {
+    return fetch(`${BASE_URL}/${searchQuery}`).then(response => response.json());
 }
 
 export default { fetchCountry };
